61f17f92722f37e291069ee43dcadbcd3d931d83
Mauricio Robayo <rfmajo@gmail.com>
2021-05-16 06:42:56 -0500

    feat: proxy youtube api get requests
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
 import type {HttpFunction} from '@google-cloud/functions-framework/build/src/functions';
+import axios from 'axios';
 
-export const helloWorld: HttpFunction = (req, res) => {
-  res.send('Hello, World');
+const BASE_URL = 'https://www.googleapis.com/youtube/v3';
+
+export const youtubeAPI: HttpFunction = async (req, res) => {
+  if (!process.env.YOUTUBE_API_KEY) {
+    console.log('Missing YOUTUBE_API_KEY env var');
+    res.status(500).send('Missing YOUTUBE_API_KEY');
+    return;
+  }
+
+  try {
+    const response = await axios.get(
+      `${BASE_URL}${req.originalUrl}&key=${process.env.YOUTUBE_API_KEY}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.log(error);
+    if (error.response?.status) {
+      res.status(error.response.status);
+    }
+    res.json(error);
+  }
 };
